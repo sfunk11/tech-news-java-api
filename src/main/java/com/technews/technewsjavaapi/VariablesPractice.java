9e2c9ba0c209b4355a6a43ccbaa3944a7147b67c
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-05-04 11:42:12 -0400

    sample exercises to get started

---
+++ src/main/java/com/technews/technewsjavaapi/VariablesPractice.java
@@ -0,0 +1,14 @@
+package com.technews.technewsjavaapi;
+
+public class VariablesPractice {
+	public static void main(String[] args) {
+		int num1= 24;
+		double num2 = 25.43;
+		double sum = num1 + num2;
+			System.out.println(sum);
+		
+		String str = "10.43";
+		String sumStr = num1 + str;
+			System.out.println(sumStr);
+	}
+}

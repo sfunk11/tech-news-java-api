ba3d706f6b8097cf067fba3c15aaba2917e2d40f
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-05-24 14:35:46 -0400

    refactored

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -1,4 +1,4 @@
-package com.technews.technewsjavaapi;
+package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
 	}
 
